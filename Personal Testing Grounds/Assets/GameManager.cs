806595d2bf9e49112018133938a08f819cfe9e6b
Kjip <kjipgamer@gmail.com>
2020-07-23 18:25:31 +0200

    Added path blockers and basic UI

---
+++ Personal Testing Grounds/Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Tower[] towers;
+    GameObject towerManager;
+
+    private void Start()
+    {
+        towerManager = GameObject.Find("TowerManager");
+    }
+
+    public void BuyTower(int index)
+    {
+        // Check if player has enough credits
+        if (towers.Length >= index)
+        {
+            GameObject newTower = Instantiate(towers[index].modelPrefab, towerManager.transform);
+            newTower.GetComponentInChildren<TowerScript>()._selected = true;
+            // Close menu
+        }
+    }
+}
+
+[System.Serializable]
+public class Tower
+{
+    public string name;
+    public string description;
+    public float cost;
+    public float damage;
+    public GameObject modelPrefab;
+}
--- Personal Testing Grounds/Assets/PathfindFollower.cs
+++ Personal Testing Grounds/Assets/PathfindFollower.cs
@@ -10,10 +10,18 @@ public class PathfindFollower : MonoBehaviour
     public float speed;
     public float pointReachedDelay; // How long the object waits after reaching a point
     public float checkDistance;
+    public float yOffset = 1f;
+
+    Vector3 Offset;
 
     int currentPathPoint;
     float lerp;
     float distanceBetweenPoints;
+
+    private void Start()
+    {
+        Offset = new Vector3(0, yOffset, 0);
+    }
 
     void Update()
     {
@@ -26,16 +34,16 @@ public class PathfindFollower : MonoBehaviour
         if (hasStarted && currentPathPoint + 1 < path.pathPoints.Count)
         {
             //Calculate the distance between the points so the speed is uniform
-            distanceBetweenPoints = Vector3.Distance(path.pathPoints[currentPathPoint], path.pathPoints[currentPathPoint + 1]);
+            distanceBetweenPoints = Vector3.Distance(path.pathPoints[currentPathPoint] + Offset, path.pathPoints[currentPathPoint + 1] + Offset);
 
             lerp += Time.deltaTime / speed / distanceBetweenPoints;
 
             // Set transform values
-            transform.position = Vector3.Lerp(path.pathPoints[currentPathPoint], path.pathPoints[currentPathPoint + 1], lerp);
-            transform.LookAt(path.pathPoints[currentPathPoint + 1]);
+            transform.position = Vector3.Lerp(path.pathPoints[currentPathPoint] + Offset, path.pathPoints[currentPathPoint + 1] + Offset, lerp);
+            transform.LookAt(path.pathPoints[currentPathPoint + 1] + Offset);
 
             // If destination is reached, go to next point
-            if (Vector3.Distance(transform.position, path.pathPoints[currentPathPoint + 1]) <= 0 + checkDistance)
+            if (Vector3.Distance(transform.position, path.pathPoints[currentPathPoint + 1] + Offset) <= 0 + checkDistance)
             {
                 currentPathPoint++;
                 lerp = 0;
