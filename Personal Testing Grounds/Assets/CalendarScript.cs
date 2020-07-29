455902c0f8f2772e25a3366f20c25805f343e1b0
Kjip <kjipgamer@gmail.com>
2020-07-29 12:19:11 +0200

    Added life calendar testing

---
+++ Personal Testing Grounds/Assets/CalendarScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CalendarScript : MonoBehaviour
+{
+
+    public int birthYear;
+    public int birthMonth;
+
+    int monthsPassed;
+
+    private void Start()
+    {
+        int yearsPassed = System.DateTime.Now.Year - birthYear;
+        int monthsPassedInYear = System.DateTime.Now.Month - birthMonth;
+
+        monthsPassed = yearsPassed * 12 + monthsPassedInYear;
+
+        MonthScript[] months = transform.GetComponentsInChildren<MonthScript>();
+        Debug.Log(months.Length);
+
+        for (int i = 0; i < monthsPassed; i++)
+        {
+            if (i < months.Length)
+                months[i].HasPassed();
+        }
+    }
+}
