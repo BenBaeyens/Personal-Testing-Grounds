1b6af1ebd7302601f6a140176bf7cc971cd8cd43
Kjip <kjipgamer@gmail.com>
2020-08-03 16:41:59 +0200

    New project: salary writer
    takes input from three files and combines them into one

---
+++ Personal Testing Grounds/Assets/FileWrite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[ExecuteInEditMode]
+public class FileWrite : MonoBehaviour
+{
+    public string writePath = "Assets/Resources/Salary.txt";
+    public string wagesPath = "Assets/Resources/Wages.txt";
+    public string employeesPath = "Assets/Resources/Employees.txt";
+    public string hoursWorkedPath = "Assets/Resources/HoursWorked.txt";
+
+
+    [Header("WRITE BUTTON:")]
+    public bool write = false;
+
+    private void Update()
+    {
+        if (write)
+        {
+            write = false;
+            FileWriter.WriteString(wagesPath, employeesPath, hoursWorkedPath, writePath);
+        }
+    }
+}
---
+++ Personal Testing Grounds/Assets/FileWriter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+
+public class FileWriter
+{
+    [MenuItem("Tools/Write file")]
+    public static void WriteString(string wagesPath, string employeesPath, string hoursWorkedPath, string salaryPath)
+    {
+
+        //Write some text to the test.txt file
+
+        int Lines = File.ReadAllLines(wagesPath).Length;
+        StreamReader wagesReader = new StreamReader(wagesPath);
+        StreamReader employeesReader = new StreamReader(employeesPath);
+        StreamReader hoursWorkedReader = new StreamReader(hoursWorkedPath);
+        StreamWriter salaryWriter = new StreamWriter(salaryPath);
+
+
+        List<float> values = new List<float>();
+        for (int i = 0; i < Lines; i++)
+        {
+            values.Add(float.Parse(wagesReader.ReadLine()) * float.Parse(hoursWorkedReader.ReadLine()));
+            salaryWriter.Write("");
+            salaryWriter.WriteLine(employeesReader.ReadLine() + " - " + values[i]);
+        }
+
+        wagesReader.Close();
+        employeesReader.Close();
+        hoursWorkedReader.Close();
+        salaryWriter.Close();
+    }
+}
