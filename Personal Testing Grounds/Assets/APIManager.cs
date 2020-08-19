105d8ae203cf8336770cc1e771c346b831d327e0
Kjip <kjipgamer@gmail.com>
2020-08-19 18:53:09 +0200

    API

---
+++ Personal Testing Grounds/Assets/APIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class APIManager : MonoBehaviour
+{
+    public string URL = "https://benbaeyens.com";
+    public Text response;
+
+
+    private void Start()
+    {
+        Request();
+    }
+
+    public void Request()
+    {
+        WWW request = new WWW(URL);
+        StartCoroutine(OnResponse(request));
+    }
+
+    IEnumerator OnResponse(WWW req)
+    {
+        yield return req;
+
+        response.text = req.text;
+    }
+}
